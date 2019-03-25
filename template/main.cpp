8d7fd8dea03fd56b6d387cc5d0c90f58a6c87a31
Paulinion <44272219+Paulinion@users.noreply.github.com>
2019-03-25 23:17:43 +0300

    Update main.cpp

--- template/main.cpp
+++ template/main.cpp
@@ -1,13 +1,22 @@
-#include <QCoreApplication>
-#include <iostream>
-
+#include<iostream>
+#include<string>
+#include<iomanip>
 using namespace std;
 
-int main(int argc, char *argv[])
-{
-    QCoreApplication a(argc, argv);
-
-    cout << "Hello world!" << endl;
-
-    return a.exec();
+int main()
+{int NumberOfWords;
+    cin >> NumberOfWords;
+    string str1;
+    unsigned long long size;
+    for(int i = 0 ; i < NumberOfWords ; ++i){
+        cin >> str1;
+        size = str1.length();
+        if (size <= 10){
+            cout << str1;
+        }
+        else
+            cout << str1[0] << size-2 << str1[size - 1];
+    }
+    return 0;
 }
+
